150cf19328f85628fd3f6bc3f48fa21f39fefe6c
George Talnagi <gtalnagi@yahoo.com>
2020-08-13 01:05:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,9 @@ class Transfer
   attr_accessor :sender, :receiver, :transfer_amount, :status
   
   def initialize(sender, receiver, transfer_amount)
-  
+  @sender = sender 
+  @receiver = receiver
+  @transfer_amount = transfer_amount
   end 
   
 
